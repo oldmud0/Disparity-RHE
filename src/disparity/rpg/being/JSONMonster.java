bf120b7679d541cae0f5d1b9192b7125dd8852ce
oldmud0 <oldmud0@private.email>
2014-02-04 12:44:38 -0600

    Added a semi-faulty JSONMonster class

--- src/disparity/rpg/being/JSONMonster.java
+++ src/disparity/rpg/being/JSONMonster.java
@@ -1,41 +1,20 @@
 package disparity.rpg.being;
-
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
 import org.json.JSONTokener;
-import org.json.JSONObject;
 import org.json.JSONArray;
-import org.json.JSONString;
+import org.json.JSONObject;
 
 public class JSONMonster{
-	public Monster readMonster(String monsterName){
-		File f = new File(monsterName + ".JSON");
-		JSONArray a = (JSONArray) parser.parse(new FileReader(f));
+	public static Monster readMonster(String monsterName){
+		File file = new File(monsterName + ".JSON");
+		
+		JSONArray monsters = new JSONArray(new JSONTokener(new FileReader("disparity/rpg/JSONfiles/monsters.JSON")));
 
-		for(JSONObject m : a){
-			JSONObject monster = (JSONObject) m;
-
-			String name = (String) m.get("name");
-			for(Object r : m.get("race")){
-				String raceName = r.get("race");
-			}
-			int str = m.get("str");
-			int agi = m.get("agi");
-			int wis = m.get("wis");
-			int con = m.get("con";)
-			for(Object h : m.get("helm")){
-				String helmName = h.get("name");
-				int helmBaseDef = h.get("base_def");
-			for(Object c : m.get("chest")){
-				String chestName = c.get("name");
-				int chestBaseDef = c.get("base_def");
-			for(Object l : m.get("legs")){
-				String legsName = m.get("name");
-				int legsBaseDef = m.get
-			}
-						
-			}
-			}
-			}
+		for(int i = 0; i < monsters.length(); i++){
+			JSONObject m = monsters.getJSONObject(i);
+			
+			
 		}
 	}
 }
